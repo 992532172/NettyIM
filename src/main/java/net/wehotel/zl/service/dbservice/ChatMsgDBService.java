f5586c8ca61c1334766223b21098f271b975e0ad
liang.zhang <liang.zhang@bestwehotel.com>
2017-07-28 10:10:56 +0800

    通过消息队列处理即时消息

--- src/main/java/net/wehotel/zl/service/dbservice/ChatMsgDBService.java
+++ src/main/java/net/wehotel/zl/service/dbservice/ChatMsgDBService.java
@@ -2,6 +2,7 @@ package net.wehotel.zl.service.dbservice;
 
 import java.util.List;
 
+import net.wehotel.zl.api.domain.ChatMsgDomain;
 import net.wehotel.zl.db.dao.ChatMsgInfoMapper;
 import net.wehotel.zl.db.entity.ChatMsgInfo;
 import net.wehotel.zl.db.entity.ChatMsgInfoExample;
@@ -17,7 +18,18 @@ public class ChatMsgDBService {
     public void insert(ChatMsgInfo record){
         chatMsgInfoMapper.insert(record);
     }
-    
+
+    public void saveChatMsg(ChatMsgDomain domain){
+        ChatMsgInfo record = new ChatMsgInfo();
+        record.setMsgcontent(domain.getMsgcontent());
+        record.setMsgtype(domain.getMsgtype());
+        record.setReceiverid(domain.getReceiverid());
+        record.setSenderid(domain.getSenderid());
+        record.setSendtime(domain.getSendtime());
+        record.setSpeakerid(domain.getSpeakerid());
+        chatMsgInfoMapper.insert(record);
+    }
+
     public List<ChatMsgInfo> findMsgByReceiver(String receiverid){
         ChatMsgInfoExample example = new ChatMsgInfoExample();
         example.createCriteria().andReceiveridEqualTo(receiverid);
