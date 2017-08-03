502c7cf164005cb0e2ea3b744348c6ad7e666e91
liang.zhang <liang.zhang@bestwehotel.com>
2017-08-03 16:24:36 +0800

    日志补充

--- src/main/java/net/wehotel/zl/netty/factory/NettyConfig.java
+++ src/main/java/net/wehotel/zl/netty/factory/NettyConfig.java
@@ -4,8 +4,11 @@ import io.netty.channel.ChannelInboundHandler;
 import io.netty.channel.ChannelOutboundHandler;
 import net.wehotel.zl.netty.SpecficLengthDecoder;
 import net.wehotel.zl.netty.SpecficLengthEncoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NettyConfig {
+    private Logger logger = LoggerFactory.getLogger(NettyConfig.class);
     private Integer port = 9990;
     private ChannelInboundHandler childHandler;
     private ChannelInboundHandler msgHandler;
@@ -14,12 +17,15 @@ public class NettyConfig {
     
     public void initConfig() {
         if (childHandler == null) {// 默认childhandler
+            logger.debug("未配置childHandler,使用默认的net.wehotel.zl.netty.factory.SimpleChannelInitializer");
             childHandler = new SimpleChannelInitializer();
         }
         if (msgEncoder == null) {// 默认编码器（自定义指定长度的消息编码器）
+            logger.debug("未配置消息编码器,使用默认的net.wehotel.zl.netty.SpecficLengthEncoder");
             msgEncoder = new SpecficLengthEncoder();
         }
         if (msgDecoder == null) {// 默认解码器（自定义指定长度的消息解码器）
+            logger.debug("未配置消息解码器,使用默认的net.wehotel.zl.netty.SpecficLengthDecoder");
             msgDecoder = new SpecficLengthDecoder();
         }
         if (msgHandler == null) {// 必须绑定一个消息处理器
